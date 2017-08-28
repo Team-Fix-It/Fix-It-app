986f29b11a1eb68550157fc87dd7cb89262334ef
Benjaminhaesemeyer <benjaminhaesemeyer@gmail.com>
2017-08-28 15:49:39 -0500

    GET complete

--- server/routes/events.router.js
+++ server/routes/events.router.js
@@ -6,6 +6,42 @@ var pool = require('../modules/pool.js');
 var ADMIN = 1;
 var USER = 2;
 
+
+//Get for all events for that user to show up on the DOM
+router.get('/', function(req, res){
+  if(req.isAuthenticated()) {
+    // errorConnecting is bool, db is what we query against,
+    // done is a function that we call when we're done
+    pool.connect(function(errorConnectingToDatabase, db, done){
+      if(errorConnectingToDatabase) {
+        console.log('Error connecting to the database.');
+        res.sendStatus(500);
+      } else {
+        //method that passport puts on the req object returns T or F
+        // Now we're going to GET things from the db
+        var queryText = 'SELECT * FROM "events"';
+        // errorMakingQuery is a bool, result is an object
+        db.query(queryText, [req.user.id], function(errorMakingQuery, result){
+          done();
+          if(errorMakingQuery) {
+            console.log('Attempted to query with', queryText);
+            console.log('Error making query');
+            res.sendStatus(500);
+          } else {
+            // console.log(result);
+            // Send back the results
+            var data = {events: result.rows};
+            res.send(data);
+          }
+        }); // end query
+
+      } // end else
+    }); // end pool
+  } else {
+    res.sendStatus(401);
+  }
+}); // end of GET
+
 // Create a new event
     router.post('/create/', function(req, res){
       var ev = req.body;

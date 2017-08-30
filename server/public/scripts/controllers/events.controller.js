bc114ab2c4eef3be255f0e17bc883ad3713cebb5
Benjaminhaesemeyer <benjaminhaesemeyer@gmail.com>
2017-08-30 14:28:06 -0500

    feature completed, cleaned up event controller, added code comments

--- server/public/scripts/controllers/events.controller.js
+++ server/public/scripts/controllers/events.controller.js
@@ -3,44 +3,55 @@ myApp.controller('EventController', function($location, $http, UserAuthService, 
   var vm = this;
   vm.Service = UserAuthService;
   vm.data = EventService.data;
+  vm.eventService = EventService;
 
+  //retrieving the info in the events table when this controller is loaded
   getEvents();
 
+              ///* CRUD operations *///
+
+  //Create a new event to add to the Events table
   vm.addEvent = function(newEvent){
-    console.log('addEvent function clicked');
-    //Here is the post request to send new event values to database
-    console.log(newEvent);
+    console.log('addEvent function clicked', newEvent);
     $http.post('/events/create', newEvent)
     .then(function(response){
       console.log('added event', response);
     });
   };
 
-  //access from the view
-  vm.editEvent = function(selectedEvent){
-    console.log( 'in editEvents functon', selectedEvent);
-    // ajax call to server to get tasks
-    $http.put('/events/edit', selectedEvent).then(function(selectedEvent){
-      getEvents();
-    }); // end success
-  }; // end editEvent
-
-  vm.deleteThisEvent = function(selectedEvent){
-    console.log( 'in deleteEvents function', selectedEvent);
-    vm.data.selectedEvent = selectedEvent;
-    // ajax call to server to get tasks
-    $http.delete('/events/edit/' + selectedEvent.id).then(function(selectedEvent){
-      getEvents();
-    }); // end success
-  };// end deleteThisEvent
-
+  //Read all of the events in the Events table
   function getEvents(){
     console.log( 'in getEvents function' );
-    // ajax call to server to get tasks
     $http.get('/events').then(function(response){
       vm.eventObject = response.data;
       console.log('events.controller vmeventObject', vm.eventObject);
     }); // end success
   } // end getEvents
+
+  //Update an event in the Events table
+  vm.editEvent = function(selectedEvent){
+    console.log( 'in editEvents functon', selectedEvent);
+    $http.put('/events/edit', selectedEvent).then(function(selectedEvent){
+      getEvents();
+    }); // end success
+  }; // end editEvent
+
+  //Delete an event from the Events table
+  vm.deleteThisEvent = function(selectedEvent){
+    console.log( 'in deleteEvents function', selectedEvent);
+    vm.data.selectedEvent = selectedEvent;
+    $http.delete('/events/edit/' + selectedEvent.id).then(function(selectedEvent){
+      getEvents();
+    }); // end success
+  };// end deleteThisEvent
+
+
+  //passing the selectedEvent into the variable called currentEvent which is located in the EventService.
+  vm.storeThisEvent = function(selectedEvent){
+    console.log('in storeThisEvent function', selectedEvent);
+    vm.eventService.currentEvent = selectedEvent;
+    $location.url('/events/checkIn');
+  };
+
 
 });
--- server/public/scripts/services/event.service.js
+++ server/public/scripts/services/event.service.js
@@ -2,10 +2,13 @@ myApp.factory('EventService', function($http, $location){
   // console.log('EventService Loaded');
 
   var data = {selectedEvent: {event_name: 'test'}};
+  
+  //Empty object for storing the selected event to update attendance on
+  var currentEvent = {};
 
   return {
     data : data,
-
+    currentEvent :currentEvent
 
 };
 });

b91486ef431ea4b10735c7f67e19a1da1f21fb9f
Emily Kuplic <emilykuplic@gmail.com>
2017-09-01 10:53:06 -0500

    newsletter post route set up need to add multiple inputs to mdDialog

--- server/routes/index.router.js
+++ server/routes/index.router.js
@@ -49,6 +49,46 @@ router.get('/events', function(req, res){
   // }
 }); // end of GET
 
+//Post to add email to database to get the newsletter
+//Post for the admin to add a new volunteer to the database
+router.post('/newsletter', function(req, res){
+  var newsletter = req.body;
+  console.log('Post route called to', email);
+  // if(req.isAuthenticated()) {
+    // errorConnecting is bool, db is what we query against,
+    // done is a function that we call when we're done
+    pool.connect(function(errorConnectingToDatabase, db, done){
+      if(errorConnectingToDatabase) {
+        console.log('Error connecting to the database.', req.body);
+        res.sendStatus(500);
+      } else {
+        //method that passport puts on the req object returns T or F
+        // Now we're going to GET things from the db
+        var queryText = 'INSERT INTO "email" ("first_name", "last_name", ' +
+        '"email")' +
+            ' VALUES ($1, $2, $3);';
+        // errorMakingQuery is a bool, result is an object
+        db.query(queryText, [newsletter.first_name, newsletter.last_name, newsletter.email], function(errorMakingQuery, result){
+          done();
+          if(errorMakingQuery) {
+            console.log('Attempted to query with', queryText);
+            console.log('Error making query');
+            res.sendStatus(500);
+          } else {
+            // console.log(result);
+            // Send back the results
+            var data = {newletter: result.rows};
+            res.send(data);
+          }
+        }); // end query
+
+      } // end else
+    }); // end pool
+  // } else {
+  //   res.sendStatus(401);
+  // }
+}); // end of POST
+
 
 
 // Handle index file separately

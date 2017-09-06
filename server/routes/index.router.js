407354a18631ebe8cba8257c119cd83938954ee0
Emily Kuplic <emilykuplic@gmail.com>
2017-09-06 16:16:22 -0500

    rsvp route working

--- server/routes/index.router.js
+++ server/routes/index.router.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var path = require('path');
+var pool = require('../modules/pool.js');
+var ADMIN = 1;
+var USER = 2;
 
 // Login Stuff
 router.get('/auth/google', passport.authenticate('google', {scope : ['openid', 'email'], prompt: 'select_account'}));
@@ -14,40 +17,6 @@ router.get('/auth/google/callback',
   });
 
 //Landing Page Stuff
-//Get for all events for that user to show up on the DOM
-router.get('/events', function(req, res){
-  // if(req.isAuthenticated()) {
-  // errorConnecting is bool, db is what we query against,
-  // done is a function that we call when we're done
-  pool.connect(function(errorConnectingToDatabase, db, done){
-    if(errorConnectingToDatabase) {
-      console.log('Error connecting to the database.');
-      res.sendStatus(500);
-    } else {
-      //method that passport puts on the req object returns T or F
-      // Now we're going to GET things from the db
-      var queryText = 'SELECT * FROM "events"';
-      // errorMakingQuery is a bool, result is an object
-      db.query(queryText, function(errorMakingQuery, result){
-        done();
-        if(errorMakingQuery) {
-          console.log('Attempted to query with', queryText);
-          console.log('Error making query');
-          res.sendStatus(500);
-        } else {
-          // console.log(result);
-          // Send back the results
-          var data = {events: result.rows};
-          res.send(data);
-        }
-      }); // end query
-
-    } // end else
-  }); // end pool
-  // } else {
-  //   res.sendStatus(401);
-  // }
-}); // end of GET
 
 //Post to add email to database to get the newsletter
 //Post for the admin to add a new volunteer to the database
@@ -99,7 +68,7 @@ router.post('/rsvp', function(req, res){
     // done is a function that we call when we're done
     pool.connect(function(errorConnectingToDatabase, db, done){
       if(errorConnectingToDatabase) {
-        console.log('Error connecting to the database.', req.body);
+        console.log('Error connecting to the database.', errorConnectingToDatabase);
         res.sendStatus(500);
       } else {
         var queryText = 'INSERT INTO "rsvp" ("event_id", "volunteer_id", "response") VALUES ($1, $2, $3)';

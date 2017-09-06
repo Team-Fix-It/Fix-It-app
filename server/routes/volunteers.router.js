58f8cfe4d76f7a441f387638a218fe75f5f24d0e
Anne Kennedy <annekennedy@Annes-MacBook-Pro-2.local>
2017-09-06 16:48:53 -0500

    finished put route

--- server/routes/volunteers.router.js
+++ server/routes/volunteers.router.js
@@ -77,6 +77,45 @@ router.get('/getSkills', function(req, res){
   // }
 }); // end of GET
 
+router.put('/edit/', function(req, res){
+  var volunteer = req.body;
+  console.log('Put route called to event of', volunteer);
+  // errorConnecting is bool, db is what we query against,
+  // done is a function that we call when we're done
+  pool.connect(function(errorConnectingToDatabase, db, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.', people);
+      res.sendStatus(500);
+    } else {
+      // if(req.isAuthenticated()) {
+      // We connected to the database!!!
+      // Now we're going to GET things from the db
+      var queryText = 'UPDATE "volunteers" SET "first_name"=$1, "last_name"=$2, "email"=$3, ' +
+      '"phone"=$4, "organization"=$5, "role"=$6, "status"=$7, "heard_about"=$8, "follow_up"=$9,' +
+       '"why_volunteer"=$10, "previous_experience"=$11 WHERE "id"= $12' ;
+      // errorMakingQuery is a bool, result is an object
+      db.query(queryText,[volunteer.first_name, volunteer.last_name, volunteer.email, volunteer.phone,
+        volunteer.organization, volunteer.role, volunteer.status, volunteer.heard_about,
+        volunteer.follow_up, volunteer.why_volunteer, volunteer.previous_experience, volunteer.id], function(errorMakingQuery, result){
+          done();
+          if(errorMakingQuery) {
+            console.log('Attempted to query with', queryText);
+            console.log('Error making query', errorMakingQuery);
+            res.sendStatus(500);
+          } else {
+            // console.log(result);
+            // Send back the results
+            var data = {volunteers: result.rows};
+            res.send(data);
+          }
+        }); // end query
+        // } else {
+        //   res.sendStatus(401);
+        // }
+      } // end if
+    }); // end pool
+  }); // end of PUT - edit event admin
+
 //Post for the admin to add a new volunteer to the database
 router.post('/add', function(req, res){
   var av = req.body;

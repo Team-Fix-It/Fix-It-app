4cf189aa37b6ca5cc12bda34011411ec35b72a85
Anne Kennedy <annekennedy@Annes-MacBook-Pro-2.local>
2017-09-07 11:10:13 -0500

    added material to create event and expanded volunteer table

--- server/public/scripts/controllers/volunteers.controller.js
+++ server/public/scripts/controllers/volunteers.controller.js
@@ -28,6 +28,14 @@ myApp.controller('VolunteersController', function($location, $http, UserAuthServ
       }); // end success
     }; // end editVolunteer
 
+    vm.seeMoreClick = function() {
+      $location.path('/volunteersEdit');
+    };
+
+    vm.goBackClick = function() {
+      $location.path('/volunteers');
+    };
+
     // vm.addVolunteer = function (volunteer){
     //   console.log( 'in addVolunteer function' );
     //   // ajax call to server to get tasks
